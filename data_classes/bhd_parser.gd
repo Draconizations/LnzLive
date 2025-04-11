e43c7bd5b6f6bb50230008b67fba0154c91b2a36
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-04-11 11:48:04 -0400

    Update bhd_parser.gd

--- data_classes/bhd_parser.gd
+++ data_classes/bhd_parser.gd
@@ -9,28 +9,28 @@ var ball_sizes = []
 
 func _init(file_path):
 	self.file_path = file_path
-	print("Initializing BhdParser with file: ", file_path)
+	#print("Initializing BhdParser with file: ", file_path)
 	var file = File.new()
 	file.open(file_path, File.READ)
 	frames_offset = file.get_16()
-	print("Frames Offset: ", frames_offset)
+	#print("Frames Offset: ", frames_offset)
 	file.get_32()
 	num_balls = file.get_16()
-	print("Number of Balls: ", num_balls)
+	#print("Number of Balls: ", num_balls)
 	file.seek(file.get_position() + 30)
 
 	if "baby" in file_path.to_lower():
 		for i in range(num_balls):
 			ball_sizes.append(file.get_16()) 
-			print("Ball Size[", i, "]: ", ball_sizes[i])
+			#print("Ball Size[", i, "]: ", ball_sizes[i])
 		file.seek(438)
 	else:
 		for i in range(num_balls):
 			ball_sizes.append(file.get_16())
-			print("Ball Size[", i, "]: ", ball_sizes[i])
+			#print("Ball Size[", i, "]: ", ball_sizes[i])
 	
 	var animation_count = file.get_16()
-	print("Number of Animations: ", animation_count)
+	#print("Number of Animations: ", animation_count)
 	
 	for i in range(animation_count):
 		var start = 0
@@ -48,7 +48,7 @@ func get_frame_offsets_for(index: int):
 	var result = []
 
 	if index >= animation_ranges.size():
-		print("Invalid index: ", index, ". Returning empty result.")
+		#print("Invalid index: ", index, ". Returning empty result.")
 		return result
 
 	var anim_range = animation_ranges[index]
