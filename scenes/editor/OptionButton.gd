8fee5a9f7a8ace00c57c8be12fbe78b49bd650e5
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-06-22 00:57:52 -0400

    Hover menu options

--- scenes/editor/OptionButton.gd
+++ scenes/editor/OptionButton.gd
@@ -1,18 +1,31 @@
 extends Button
 
+onready var popup = $PopupPanel
+var close_timer := Timer.new()
+
 func _ready():
-	var popup = $PopupPanel
 	popup.hide()
-	connect("pressed", self, "_on_button_pressed")
 
-func _on_button_pressed():
-	var popup = $PopupPanel
+	add_child(close_timer)
+	close_timer.set_wait_time(2.0)
+	close_timer.set_one_shot(true)
+	close_timer.connect("timeout", self, "_on_close_timer_timeout")
 
-	if popup.visible:
-		popup.hide()
-	else:
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
+	popup.connect("mouse_entered", self, "_on_mouse_entered")
+	popup.connect("mouse_exited", self, "_on_mouse_exited")
+
+func _on_mouse_entered():
+	close_timer.stop()
+	if not popup.visible:
 		var button_pos = rect_global_position
 		var button_size = rect_size
-
 		popup.set_position(Vector2(button_pos.x, button_pos.y + button_size.y))
-		popup.show()
+		popup.popup()
+
+func _on_mouse_exited():
+	close_timer.start()
+
+func _on_close_timer_timeout():
+	popup.hide()
