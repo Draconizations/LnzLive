e3f2724be02a4003a4b7716249ebd27749110e02
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-08-05 20:47:26 -0400

    Delete GuideLinkButton.gd

--- scenes/editor/GuideLinkButton.gd
+++
@@ -1,7 +0,0 @@
-extends LinkButton
-
-func _ready():
-	connect("pressed", self, "_on_GuideLinkButton_pressed")
-
-func _on_GuideLinkButton_pressed():
-	OS.shell_open("https://github.com/tabbzi/LnzLive/blob/master/GUIDE.md")
