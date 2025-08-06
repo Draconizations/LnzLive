b55db978bd793a2dbdb3e4282b62c4a2b6834408
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-08-05 20:47:31 -0400

    Create MenuButton.gd

---
+++ scenes/editor/MenuButton.gd
@@ -0,0 +1,13 @@
+extends LinkButton
+
+func _ready():
+	connect("pressed", self, "_on_GuideLinkButton_pressed")
+
+func _on_GuideLinkButton_pressed():
+	OS.shell_open("https://github.com/tabbzi/LnzLive/blob/master/GUIDE.md")
+	
+func _on_CarolynHornLinkButton_pressed():
+	OS.shell_open("https://github.com/melissamcewen/carolyns-bible")
+
+func _on_HelpPopupButton_pressed():
+	print("blash")
