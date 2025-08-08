a9942b71430359ea98083b47211d0e8e0f0119b8
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-08-07 22:09:02 -0400

    Update MenuButton.gd

--- scenes/editor/MenuButton.gd
+++ scenes/editor/MenuButton.gd
@@ -2,6 +2,7 @@ extends LinkButton
 
 func _ready():
 	connect("pressed", self, "_on_GuideLinkButton_pressed")
+	connect("pressed", self, "_on_CarolynHornLinkButton_pressed")
 
 func _on_GuideLinkButton_pressed():
 	OS.shell_open("https://github.com/tabbzi/LnzLive/blob/master/GUIDE.md")
