36f77813018b6bf4f95b85a0e62a79025ff60ad5
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-08-13 23:37:16 -0400

    Update UserSettings.gd

--- scenes/editor/UserSettings.gd
+++ scenes/editor/UserSettings.gd
@@ -2,9 +2,17 @@ extends Control
 
 onready var color_picker = get_node("HSplitContainer/HSplitContainer/PetViewContainer/VBoxContainer/DropDownMenu/BackgroundColorPickerButton")
 onready var color_rect = get_node("BackgroundColorRect")
+onready var view_palette_button = get_node("HSplitContainer/HSplitContainer/PetViewContainer/VBoxContainer/DropDownMenu/ToolOptionButton/PopupPanel/ToolOptionContainer/ViewPaletteButton")
+onready var palette_viewer_popup = get_node("PaletteViewerPopup")
+
 
 func _ready():
 	color_picker.connect("color_changed", self, "_on_color_changed")
+	view_palette_button.connect("pressed", self, "_on_ViewPaletteButton_pressed")
 
 func _on_color_changed(new_color: Color):
 	color_rect.color = new_color
+
+func _on_ViewPaletteButton_pressed():
+	palette_viewer_popup.populate_colors()
+	palette_viewer_popup.popup_centered_minsize()
