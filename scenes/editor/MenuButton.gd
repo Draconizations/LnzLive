b7ab62aff81050af5dbb6fda37faf7550fd5932a
Kathleen Morrill Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-11-07 09:32:47 -0500

    Fix link buttons

--- scenes/editor/MenuButton.gd
+++ scenes/editor/MenuButton.gd
@@ -1,11 +1,10 @@
 extends LinkButton
 
-func _ready():
-	connect("pressed", self, "_on_GuideLinkButton_pressed")
-	connect("pressed", self, "_on_CarolynHornLinkButton_pressed")
+export var url: String = ""
 
-func _on_GuideLinkButton_pressed():
-	OS.shell_open("https://github.com/tabbzi/LnzLive/blob/master/GUIDE.md")
-	
-func _on_CarolynHornLinkButton_pressed():
-	OS.shell_open("https://github.com/melissamcewen/carolyns-bible")
+func _ready():
+	connect("pressed", self, "_on_pressed")
+
+func _on_pressed():
+	if not url.empty():
+		OS.shell_open(url)
