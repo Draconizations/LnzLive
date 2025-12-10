802a6e99ec47b39ead9d8814db46ae617450fa24
Kathleen Morrill Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-12-10 08:18:09 -0500

    Set window shrink

--- scenes/editor/UserSettings.gd
+++ scenes/editor/UserSettings.gd
@@ -10,7 +10,7 @@ const SETTINGS_PATH = "user://settings.cfg"
 onready var color_picker = get_node("HSplitContainer/HSplitContainer/PetViewContainer/VBoxContainer/DropDownMenu/BackgroundColorPickerButton")
 onready var color_rect = get_node("BackgroundColorRect")
 onready var view_palette_button = get_node("HSplitContainer/HSplitContainer/PetViewContainer/VBoxContainer/DropDownMenu/ToolOptionButton/PopupPanel/ToolOptionContainer/ViewPaletteButton")
-
+onready var shrink_spinner = get_node("HSplitContainer/HSplitContainer/PetViewContainer/VBoxContainer/DropDownMenu/ShrinkSpinBox")
 onready var file_tree = get_tree().get_root().get_node("Root/SceneRoot/HSplitContainer/VBoxContainer/Tree")
 var _cached_window_size = Vector2(1024, 600)
 var _cached_window_pos = Vector2()
@@ -19,6 +19,8 @@ func _ready():
 	_cached_window_size = OS.window_size
 	_cached_window_pos = OS.window_position
 	
+	shrink_spinner.connect("value_changed", self, "_on_shrink_changed")
+
 	load_settings()
 	
 	color_picker.connect("color_changed", self, "_on_color_changed")
@@ -43,6 +45,7 @@ func save_settings():
 	config.set_value("Display", "window_size", _cached_window_size)
 	config.set_value("Display", "window_position", _cached_window_pos)
 	config.set_value("Display", "background_color", color_rect.color)
+	config.set_value("Display", "shrink", shrink_spinner.value)
 	
 	var save_err = config.save(SETTINGS_PATH)
 	if save_err != OK:
@@ -58,7 +61,11 @@ func load_settings():
 		var bg_color = config.get_value("Display", "background_color", default_color)
 		color_rect.color = bg_color
 		color_picker.color = bg_color
-		
+
+		var saved_shrink = config.get_value("Display", "shrink", 1)
+		shrink_spinner.value = saved_shrink
+		_apply_screen_shrink(saved_shrink)
+
 		var saved_size = config.get_value("Display", "window_size", null)
 		var saved_pos = config.get_value("Display", "window_position", null)
 		
@@ -87,3 +94,16 @@ func load_settings():
 
 func _on_color_changed(new_color: Color):
 	color_rect.color = new_color
+
+func _on_shrink_changed(value):
+	_apply_screen_shrink(value)
+
+func _apply_screen_shrink(shrink_value):
+	var base_size = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+	
+	get_tree().set_screen_stretch(
+		SceneTree.STRETCH_MODE_2D, 
+		SceneTree.STRETCH_ASPECT_EXPAND, 
+		base_size, 
+		shrink_value
+	)
