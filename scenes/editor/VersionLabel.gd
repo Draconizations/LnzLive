fe515566f38c7ec80c9d092aa469e7e414ff7f26
Kathleen Morrill Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-12-18 21:47:37 -0500

    Update VersionLabel.gd

--- scenes/editor/VersionLabel.gd
+++ scenes/editor/VersionLabel.gd
@@ -1,19 +1,8 @@
 extends Label
 
 func _ready():
-    var version = get_version_from_presets()
-    if version:
-        text = str(version)
-    else:
-        text = "unknown version"
-
-func get_version_from_presets():
-    var config = ConfigFile.new()
-    var err = config.load("res://export_presets.cfg")
-    
-    if err == OK:
-        var val = config.get_value("preset.1.options", "application/file_version", null)
-        return val
-    else:
-        print("Error: Could not open export_presets.cfg (Code: ", err, ")")
-        return null
+	var version = ProjectSettings.get_setting("application/config/file_version")
+	if version:
+		text = "v" + str(version)
+	else:
+		text = "v?.?.?"
