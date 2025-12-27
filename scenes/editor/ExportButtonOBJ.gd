84941daf5775dac365316e0a477293485410f199
Kathleen Morrill Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-12-27 13:06:24 -0500

    Optimize OBJ export and fix linez

--- scenes/editor/ExportButtonOBJ.gd
+++ scenes/editor/ExportButtonOBJ.gd
@@ -20,20 +20,16 @@ func _on_pressed():
 	_save_file_as(filename, content_bytes)
 
 func _export_current_model() -> PoolByteArray:
-	var obj_content = ""
+	var obj_lines = PoolStringArray()
 
-	# Set OBJ header
-	obj_content += "# Exported Petz Model\n"
-	obj_content += "o Petz\n\n"
+	obj_lines.append("# Exported Petz Model")
+	obj_lines.append("o Petz\n")
 
-	# Grab scene data from PetRoot
 	var dog = get_tree().root.get_node("Root/PetRoot/Node")
-	print("Total balls in dog.ball_map:", dog.ball_map.size())
 	var pixel_world_size = dog.pixel_world_size
 	var lines = dog.lnz.lines
 	var polys = dog.lnz.polygons
 
-	# Define visible ballz to export
 	var all_ball_nodes = []
 	var by_no = {}
 	for b in dog.ball_map.values():
@@ -42,146 +38,105 @@ func _export_current_model() -> PoolByteArray:
 				all_ball_nodes.append(b)
 				by_no[b.ball_no] = b
 
-	print("? Found", all_ball_nodes.size(), "Ball.gd nodes")
-
-	print("lines:", lines.size(), "polygons:", polys.size())
-
 	var vertex_offset = 1
+	var S_SEG = 12 
+	var S_RNG = 8  
 
-	# Define spheres for ballz
-	var S_SEG = 12 # longitude segments
-	var S_RNG = 8  # latitude segments
-
+	# BALLZ
 	for bd in all_ball_nodes:
-
-		# Get center in world coordinates
 		var center = bd.global_transform.origin
-		center.y = -center.y
-
-		# Get radius from ballz size in world units
+		#center.y = -center.y
 		var radius = bd.ball_size * pixel_world_size * 0.5
 
-		print("ball", bd.ball_no, "size", bd.ball_size)
-		print("radius", radius)
-
-		# Write vertices
+		# Vertices
 		for i in range(S_RNG + 1):
 			var phi = PI * i / S_RNG
+			var sin_phi = sin(phi)
+			var cos_phi = cos(phi)
 			for j in range(S_SEG):
 				var theta = TAU * j / S_SEG
-				var x = radius * sin(phi) * cos(theta)
-				var y = radius * cos(phi)
-				var z = radius * sin(phi) * sin(theta)
-				obj_content += "v %f %f %f\n" % [center.x + x, center.y + y, center.z + z]
+				var x = radius * sin_phi * cos(theta)
+				var y = radius * cos_phi
+				var z = radius * sin_phi * sin(theta)
+				obj_lines.append("v %f %f %f" % [center.x + x, center.y + y, center.z + z])
 		
-		# Write faces
+		# Faces
 		for i in range(S_RNG):
 			for j in range(S_SEG):
 				var a = vertex_offset + i * S_SEG + j
 				var b = vertex_offset + (i + 1) * S_SEG + j
 				var c = vertex_offset + (i + 1) * S_SEG + (j + 1) % S_SEG
 				var d = vertex_offset + i * S_SEG + (j + 1) % S_SEG
-				obj_content += "f %d %d %d\n" % [a, b, c]
-				obj_content += "f %d %d %d\n" % [a, c, d]
+				obj_lines.append("f %d %d %d" % [a, b, c])
+				obj_lines.append("f %d %d %d" % [a, c, d])
 		
-		# Track vertex index
 		vertex_offset += (S_RNG + 1) * S_SEG
 	
-	# Define cylinders for linez
-	var cyl = CylinderMesh.new()
-	cyl.radial_segments = 6
-	cyl.rings = 1
-
+	# LINEZ 
 	for ld in lines:
-
-		# Get start ballz and end ballz
 		var b1 = by_no.get(ld.start)
 		var b2 = by_no.get(ld.end)
-		if b1 == null or b2 == null:
-			continue
+		if b1 == null or b2 == null: continue
 
-		# Get start point and end point in world coordinates
-		var p1 = b1.global_transform.origin; p1.y = -p1.y
-		var p2 = b2.global_transform.origin; p2.y = -p2.y
-
-		# Define geometry between start ballz and end ballz
-		var dir = (p2 - p1).normalized()
-		var length = p1.distance_to(p2)
-		var mid = p1 + dir * (length * 0.5)
-
-		if typeof(b1.get("ball_size")) != TYPE_REAL or typeof(b2.get("ball_size")) != TYPE_REAL:
-			print("Skipping line due to invalid ball_size:", ld.start, ld.end)
-			continue
+		var p1 = b1.global_transform.origin
+		var p2 = b2.global_transform.origin
 		
-		# Define radius and scale tapers by line thickness
-		var min_radius = 0.0
-		var d1 = b1.ball_size * (ld.s_thick / 100.0)
-		var d2 = b2.ball_size * (ld.e_thick / 100.0)
-		var r1 = max(d1 * pixel_world_size * 0.5, min_radius)
-		var r2 = max(d2 * pixel_world_size * 0.5, min_radius)
-
-		print("length=", length, "   expected ball spacing=", p1.distance_to(p2))
-
-		cyl.height = length * 2
-		cyl.bottom_radius = r1
-		cyl.top_radius = r2
-
-		# Bake cylinder mesh
-		var arr2 = cyl.surface_get_arrays(0)
-		var verts2 = arr2[Mesh.ARRAY_VERTEX]
-		var idxs2 = arr2[Mesh.ARRAY_INDEX]
-
-		# Orient cylinder in direction of start ballz and end ballz
-		var up = Vector3(0, 1, 0)
-		var axis = up.cross(dir)
-		if axis.length() < 1e-4:
-			axis = Vector3(1, 0, 0)
-		var angle = acos(clamp(up.dot(dir), -1, 1))
-		var basis = Basis(Quat(axis.normalized(), angle))
-
-		# Write vertices
-		var offset = basis.xform(Vector3(0, -cyl.height * 0.5, 0))
-		for v in verts2:
-			var gv = basis.xform(v) + p1
-			obj_content += "v %f %f %f\n" % [gv.x, gv.y, gv.z]
-
-		# Write faces
-		var segs = cyl.radial_segments
-		var side_idx_limit = segs * 2 * 3
-
-		for i in range(0, side_idx_limit, 3):
-			var a = int(idxs2[i    ]) + vertex_offset
-			var b = int(idxs2[i+1]) + vertex_offset
-			var c = int(idxs2[i+2]) + vertex_offset
-			obj_content += "f %d %d %d\n" % [a, b, c]
+		var diff = p2 - p1 
+		var length = diff.length() 
+		if length < 0.0001: continue
+		var dir = diff.normalized() 
 		
-		# Track vertex index
-		vertex_offset += verts2.size()
+		var r1 = max(b1.ball_size * (ld.s_thick / 100.0) * pixel_world_size * 0.5, 0.0)
+		var r2 = max(b2.ball_size * (ld.e_thick / 100.0) * pixel_world_size * 0.5, 0.0)
 
-	# Define quads from polygonz
+		var v_up = Vector3(0, 1, 0)
+		if abs(v_up.dot(dir)) > 0.9:
+			v_up = Vector3(1, 0, 0)
+		var v_right = v_up.cross(dir).normalized()
+		var v_true_up = dir.cross(v_right).normalized()
+		
+		var segments = 6
+		
+		for i in range(segments):
+			var angle = TAU * i / segments
+			var ring_offset = (v_right * cos(angle) + v_true_up * sin(angle))
+			
+			var v1 = p1 + ring_offset * r1
+			var v2 = p2 + ring_offset * r2
+			
+			obj_lines.append("v %f %f %f" % [v1.x, v1.y, v1.z])
+			obj_lines.append("v %f %f %f" % [v2.x, v2.y, v2.z])
+		
+		for i in range(segments):
+			var i_next = (i + 1) % segments
+			var a = vertex_offset + i * 2
+			var b = vertex_offset + i * 2 + 1
+			var c = vertex_offset + i_next * 2 + 1
+			var d = vertex_offset + i_next * 2
+			
+			obj_lines.append("f %d %d %d" % [a, b, c])
+			obj_lines.append("f %d %d %d" % [a, c, d])
+		
+		vertex_offset += segments * 2
+
+	# POLYGONZ
 	for pd in polys:
-
-		# Get centers four defining ballz in world coordinates
 		var pts = []
 		for key in ["ball1", "ball2", "ball3", "ball4"]:
 			var bd = by_no.get(pd[key])
 			if bd:
 				var pp = bd.global_transform.origin
-				pp.y = -pp.y
-				pts.append(pp * pixel_world_size)
+				#pp.y = -pp.y
+				pts.append(pp)
+		
 		if pts.size() == 4:
 			for p in pts:
-				# Write vertices
-				obj_content += "v %f %f %f\n" % [p.x, p.y, p.z]
-
-			# Write faces
-			obj_content += "f %d %d %d\n" % [vertex_offset + 0, vertex_offset + 1, vertex_offset + 2]
-			obj_content += "f %d %d %d\n" % [vertex_offset + 0, vertex_offset + 2, vertex_offset + 3]
-
-			# Track vertex index
+				obj_lines.append("v %f %f %f" % [p.x, p.y, p.z])
+			obj_lines.append("f %d %d %d" % [vertex_offset, vertex_offset + 1, vertex_offset + 2])
+			obj_lines.append("f %d %d %d" % [vertex_offset, vertex_offset + 2, vertex_offset + 3])
 			vertex_offset += 4
 
-	return obj_content.to_utf8()
+	return obj_lines.join("\n").to_utf8()
 
 func _save_file_as(filename: String, content_bytes: PoolByteArray):
 	if OS.has_feature("HTML5"):
