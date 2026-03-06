9be475ee1fe638ae506fb8c0f1b1bca1a4805764
Kathleen Morrill Pirovich <34375512+tabbzi@users.noreply.github.com>
2026-03-06 00:37:57 -0500

    enum does get used

--- data_classes/section_enum.gd
+++ data_classes/section_enum.gd
@@ -2,7 +2,6 @@ class_name Section
 extends Node
 ## section_enum.gd
 ## Defines an enumeration for the different sections of a LNZ document
-## NOTE: I don't know that this gets used anymore, check if can delete
 
 enum Section {
 	BALL,
